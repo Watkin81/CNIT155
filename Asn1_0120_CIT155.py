264f9066078e0cb3df189e980d45951ec0a5d6ce
Watkinsj8120 <76190146+Watkinsj8120@users.noreply.github.com>
2022-01-26 09:41:26 -0500

    Add files via upload

---
+++ Asn1_0120_CIT155.py
@@ -0,0 +1,30 @@
+#====================================================================================
+# Jake Watkins, Thursday 11:30am
+# Watkin81@purdue.edu
+# Program shows a header with the assignment number, and then displays CIT155.
+#
+# Academic Honesty:
+# I attest that this is my original work.
+# I have not used unauthorized source code, either modified or unmodified.
+# I have not given other fellow student(s) access to my program.
+#====================================================================================
+
+def Header():
+    print("*******************************************")
+    print("*          CNIT155 Assignment 01          *")
+    print("*******************************************")
+    
+Header()
+
+print("")
+print("CCC   III   TTT")
+print("C      I     T ")
+print("C      I     T ")
+print("C      I     T ")
+print("CCC   III    T ")
+print("")
+print("11    555   555")
+print(" 1    5     5  ")
+print(" 1    555   555")
+print(" 1      5     5")
+print("111   555   555")
---
+++ Lab1_0120_HelloWorld.py
@@ -0,0 +1,20 @@
+#====================================================================================
+# Jake Watkins, Thursday 11:30am
+# Watkin81@purdue.edu
+# Program shows a header with the assignment number, and then runs hello world.
+#
+# Academic Honesty:
+# I attest that this is my original work.
+# I have not used unauthorized source code, either modified or unmodified.
+# I have not given other fellow student(s) access to my program.
+#====================================================================================
+
+def Header():
+    print("*******************************************")
+    print("*          CNIT155 Assignment 01          *")
+    print("*******************************************")
+    
+Header()
+
+print("Hello World!")
+print("This is my first Python Program!")
